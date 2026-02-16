ea1260b49eb89e8ed741e359ad8807ef501a8c34
Asep <asep.saipullah09@gmail.com>
2026-02-17 03:07:29 +0700

    Initial full stack setup with authentication (React, Node, Express, MongoDB, JWT)

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl text-blue-500 font-bold">
+        MultiCode Platform 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
